e9d86bf42658c3cb08eefedf6cadc09205847b1e
杨璐 <812708519@qq.com>
2016-05-06 16:23:28 +0800

    增加phpcms需要的文件

--- dist/js/dynamic.js
+++ dist/js/dynamic.js
@@ -13,7 +13,7 @@ $('document').ready(function () {
         var linkId = $(this).attr('data-id');
         console.log(linkId);
         $.ajax({
-            url: "index.php?m=content&c=index&a=show&catid=9&id="+46,    //请求的url地址
+            url: "index.php?m=content&c=index&a=show&catid=8&id="+linkId,    //请求的url地址
             dataType: "html",   //返回格式为json
             async: true, //请求是否异步，默认为异步，这也是ajax重要特性
             //data: { "id": linkId },    //参数值
