03389ef79c50b0d172a36f41e401f7f41be26c87
zhangjinpei <302784355@qq.com>
2016-05-06 17:53:37 +0800

    修改文件

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,4 @@
 $(document).ready(function () {
-    //导航条对应菜单高亮
-    autoActive('whichPages', 'navbar');
-    function autoActive(flagName, navbarId) {
-        var pagesValueArray = creadNavArray(navbarId);
-        autoAddClass(flagName, pagesValueArray);
-    }
-
-    //根据当前页面唯一值，判断是哪个页面，添加高亮样式
-    function autoAddClass(flagName, pagesValueArray) {
-        var pageValue = $('input[name=' + flagName + ']').val();
-        for (var i = 0, j = pagesValueArray.length; i < j; i++) {
-            if (pageValue == pagesValueArray[i]) {
-                $('#' + pagesValueArray[i]).addClass('active');
-
-            }
-        }
-    }
-
-    //将导航栏的id存入到一个数组中
-    function creadNavArray(navbarId) {
-        var navArray = [];
-        var navbar = $('#' + navbarId + ' li');
-        for (var i = 0, j = navbar.length; i < j; i++) {
-            var navbarChildrenId = navbar.eq(i).attr('id');
-            navArray.push(navbarChildrenId);
-        }
-        return navArray;
-    }
-
     //小屏幕小展开收起菜单
     $(".show-menu-icon").on("click",function () {
         if ($(".nav-menu").hasClass("show-menu")) {
